c2aae4c291997c369a60e2c504dc8846baad2e2f
xiahouxin <2786724159@qq.com>
2023-03-12 19:41:20 +0800

    字符串

---
+++ print/index.js
@@ -0,0 +1,5 @@
+// # 字符串
+let str = 'Hello world!';
+
+console.log(str.includes('ell'));  // 输出结果：true
+console.log(str.startsWith('Hel')); // 输出结果：false
--- 场景题/函数实现/index.js
+++ 场景题/函数实现/index.js
@@ -2,5 +2,10 @@
 let obj = {a:1, b:3, c:5}  
 let a=Object.fromEntries(Object.entries(obj).filter(([key,value],index)=> value>2 ))
 let obj2=Object.fromEntries([['aa',11],['bb',22],['cc',33]])
-console.log(obj2);
-console.log(Object.entries(obj));
+// console.log(obj2);
+// console.log(Object.entries(obj));
+
+// # 字符串
+let str = 'Hello world!';
+
+console.log(str.includes('o'));  // 输出结果：true
