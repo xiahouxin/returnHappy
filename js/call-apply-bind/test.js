e6d60990445b4c70bd33888efc208c1b4eb61ed6
xiahouxin <2786724159@qq.com>
2023-03-20 15:16:02 +0800

    数组对象的解构赋值

--- js/call-apply-bind/test.js
+++ js/call-apply-bind/test.js
@@ -5,8 +5,16 @@
 // let [x, y] = [1, 2]
 // console.log(x, y);
 
-let arr = [1, 2, 3, 4, 5]
+let obj={
+    a:1,
+    b:2,
+    x:11,
+}
+// let {a:x,b:y}=obj
+let {x:aa,y:bb}=obj
+console.log(aa,bb);
+// console.log(a,b);
 
-let newArr = [...arr]
-
-console.log(newArr);
+// let arr = [1, 2, 3, 4, 5]
+// let newArr = [...arr]
+// console.log(newArr);
