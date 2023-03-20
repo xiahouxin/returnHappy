847cf9d34829fbcfb6d4c2a77284658d6c7b1de7
xiahouxin <2786724159@qq.com>
2023-03-20 16:41:51 +0800

    箭头函数的this

--- js/call-apply-bind/index.js
+++ js/call-apply-bind/index.js
@@ -1,13 +1,15 @@
+// let user='xhx'
 // var a = {
 //   user: '家君',
-//   fn: function(e, ee) {
+//   fn: function(e=0, ee=0) {
 //     console.log(this.user);
 //     console.log(e + ee);
 //   }
 // }
 // // a.fn()
 // var b = a.fn
-// b.call(a, 1, 2)
+// b()
+// // b.call(a, 1, 2)
 
 
 
@@ -38,29 +40,36 @@
 // c(4, 5)
 
 var name = "windowsName";
-
+// this.name='xhx'
 var a = {
     name : "Cherry",
-
     func1: function () {
         console.log(this.name)     
     },
-
     func2: function () {
-        setTimeout( () => {
-            this.func1()
+        this.name='xhx'
+        setTimeout(() => {
+            console.log(this.name,'箭头函数');
         },100);
     }
 
 };
 
-a.func2()     // Cherry
+// a.func2()     // Cherry
+this.name='zmr'
+let bar=a.func2
+bar()
+
+// console.log(this.name,'quanju');
+setTimeout(()=>{
+    console.log(this.name,'外面');
+},1111)
 
 // -------------------------
 // fn.bind().bind(a) 等于
-let fn2 = function fn1() {
-    return function() {
-      return fn.apply()
-    }.apply(a)
-  }
-  fn2()
+// let fn2 = function fn1() {
+//     return function() {
+//       return fn.apply()
+//     }.apply(a)
+//   }
+//   fn2()
