341893d117cdeb6e140f9bb31ac127d07d3697e0
xiahouxin <2786724159@qq.com>
2023-04-01 12:08:46 +0800

    固定参数柯里化

--- 柯里化4件套/1.js
+++ 柯里化4件套/1.js
@@ -32,28 +32,31 @@
 //   };
 // };
 
-// const curry = (fn) => {
-//   let length = fn.length;
-//   return function temp() {
-//     let arg = [...arguments];
-//     if (arg.length >= length) {
-//       return fn(...arg);
-//     } else {
-//       return function () {
-//         return temp(...arg, ...arguments);
-//       };
-//     }
-//   };
-// };
-// function add(a, b, c) {
-//   return a + b + c;
-// }
+const curry = (fn) => {
+  let length = fn.length;
+  return function temp() {
+    let arg = [...arguments];
+    if (arg.length >= length) {
+      return fn(...arg);
+    } else {
+      return function () {
+        return temp(...arg, ...arguments);
+      };
+    }
+  };
+};
+function add(a, b, c) {
+  return a + b + c;
+}
 
-// let test = curry(add);
-// console.log(test(1)(2, 3));
-console.log('2'>'13');  //true
-console.log('b'<'ac');  //false
-console.log(['2','13'].sort((a,b)=>a-b));  //['2','13']
-console.log(['b','ac'].sort((a,b)=>a-b));  //[ 'b', 'ac' ]
-console.log(['ac','b'].sort((a,b)=>a-b));  //[ 'ac', 'b' ]
-console.log(['ac',2,1,'b',11,6,3].sort((a,b)=>a-b));  //['ac', 1, 2, 'b',  3, 6,11]
+let test = curry(add);
+console.log(test(1)(2, 3));
+
+
+// 字符串测试
+// console.log('2'>'13');  //true
+// console.log('b'<'ac');  //false
+// console.log(['2','13'].sort((a,b)=>a-b));  //['2','13']
+// console.log(['b','ac'].sort((a,b)=>a-b));  //[ 'b', 'ac' ]
+// console.log(['ac','b'].sort((a,b)=>a-b));  //[ 'ac', 'b' ]
+// console.log(['ac',2,1,'b',11,6,3].sort((a,b)=>a-b));  //['ac', 1, 2, 'b',  3, 6,11]
--- 柯里化4件套/2.js
+++ 柯里化4件套/2.js
@@ -1,30 +1,30 @@
- // function curry(fn) {
-      //   let arg = [...arguments].slice(1);
-      //   let temp = function temp() {
-      //     arg = [...arg, ...arguments];
-      //     return curry(fn, ...arg);
-      //   };
-      //   temp.toString = function () {
-      //     return fn.apply(null, arg);
-      //   };
-      //   return temp;
-      // }
+// function curry(fn) {
+//   let arg = [...arguments].slice(1);
+//   let temp = function temp() {
+//     arg = [...arg, ...arguments];
+//     return curry(fn, ...arg);
+//   };
+//   temp.toString = function () {
+//     return fn.apply(null, arg);
+//   };
+//   return temp;
+// }
 
-      function curry(fn) {
-        let arg = [...arguments].slice(1);
-        let temp = function temp() {
-          arg = [...arg, ...arguments];
-          return curry(fn, ...arg);
-        };
-        temp.toString = function () {
-          return fn.apply(null, arg);
-        };
-        return temp;
-      }
-      function fn() {
-        return [...arguments].reduce((pre, cru) => {
-          return pre + cru;
-        }, 0);
-      }
-      let test = curry(fn);
-      console.log(test(1, 2)(2, 3)(4).toString());
+function curry(fn) {
+  let arg = [...arguments].slice(1);
+  let temp = function temp() {
+    arg = [...arg, ...arguments];
+    return curry(fn, ...arg);
+  };
+  temp.toString = function () {
+    return fn.apply(null, arg);
+  };
+  return temp;
+}
+function fn() {
+  return [...arguments].reduce((pre, cru) => {
+    return pre + cru;
+  }, 0);
+}
+let test = curry(fn);
+console.log(test(1, 2)(2, 3)(4).toString());
