9d38db543273c6f84202d4905d334c84534d0dd9
xiahouxin <2786724159@qq.com>
2023-04-05 14:11:12 +0800

    迭代器

--- 前端基础知识手写/基础实现/28.js
+++ 前端基础知识手写/基础实现/28.js
@@ -1,31 +1,31 @@
 let arr = ["213", "asd", 2, 3, 14];
-      // Array.prototype.myinterator = function () {
-      //   i = 0;
-      //   return {
-      //     next: () => {
-      //       let a = {
-      //         value: this[i] || undefined,
-      //         done: i >= this.length - 1 ? true : false,
-      //       };
-      //       i++;
-      //       return a;
-      //     },
-      //   };
-      // };
-      Array.prototype._interator = function () {
-        let i = 0;
-        return {
-          next: () => {
-            return {
-              value: this[i++],
-              done: i < this.length ? false : true,
-            };
-          },
-        };
+// Array.prototype.myinterator = function () {
+//   i = 0;
+//   return {
+//     next: () => {
+//       let a = {
+//         value: this[i] || undefined,
+//         done: i >= this.length - 1 ? true : false,
+//       };
+//       i++;
+//       return a;
+//     },
+//   };
+// };
+Array.prototype._interator = function () {
+  let i = 0;
+  return {
+    next: () => {
+      return {
+        value: this[i++],
+        done: i < this.length ? false : true,
       };
-      let b = arr._interator();
-      console.log(b.next());
-      console.log(b.next());
-      console.log(b.next());
-      console.log(b.next());
-      console.log(b.next());
+    },
+  };
+};
+let b = arr._interator();
+console.log(b.next());
+console.log(b.next());
+console.log(b.next());
+console.log(b.next());
+console.log(b.next());
