32dff459821b7c1f019a9e630d1ed1a87374f1b9
xiahouxin <2786724159@qq.com>
2023-04-05 13:55:08 +0800

    vue-path

--- 前端基础知识手写/基础实现/21.js
+++ 前端基础知识手写/基础实现/21.js
@@ -21,3 +21,18 @@ function createNewNode(vnode) {
 
     return ele;
   }
+
+
+  //简单实现vue的更新方法，递归实现
+  function update(vNode, newNode) {
+    let children = vNode.children || {};
+    let newChildren = newNode.children || {};
+    children.forEach((childrenVnode, index) => {
+      let newChildrenVnode = newChildren[index];
+      if (childrenVnode.tag == newChildrenVnode.tag) {
+        update(childrenVnode, newChildrenVnode);
+      } else {
+        replace(childrenVnode, newChildrenVnode);
+      }
+    });
+  }
