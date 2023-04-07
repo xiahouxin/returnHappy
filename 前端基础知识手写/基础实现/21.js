a3e3376605a74ec876182ec56d6ad6f00490fb11
xiahouxin <2786724159@qq.com>
2023-04-07 20:22:46 +0800

    数组扁平化

--- 前端基础知识手写/基础实现/21.js
+++ 前端基础知识手写/基础实现/21.js
@@ -1,38 +1,37 @@
 function createNewNode(vnode) {
-    let tag = vnode.tag; //目标元素
-    let attr = vnode.attr || {}; //目标属性
-    let children = vnode.children || {}; //目标的孩子
-    if (!tag) {
-      return null;
-    }
-    //创建dom
-    let ele = document.createElement();
-    let attrName;
-    //给dom添加元素
-    for (attrName in attr) {
-      if (attr.hasOwnProperty(attrName)) {
-        ele.setAttribute(attrName, attr[attrName]);
-      }
-    }
-    //将子元素添加到dom上
-    children.forEach((children) => {
-      ele.appendChild(createNewNode(children));
-    });
-
-    return ele;
+  let tag = vnode.tag; //目标元素
+  let attr = vnode.attr || {}; //目标属性
+  let children = vnode.children || {}; //目标的孩子
+  if (!tag) {
+    return null;
   }
-
-
-  //简单实现vue的更新方法，递归实现
-  function update(vNode, newNode) {
-    let children = vNode.children || {};
-    let newChildren = newNode.children || {};
-    children.forEach((childrenVnode, index) => {
-      let newChildrenVnode = newChildren[index];
-      if (childrenVnode.tag == newChildrenVnode.tag) {
-        update(childrenVnode, newChildrenVnode);
-      } else {
-        replace(childrenVnode, newChildrenVnode);
-      }
-    });
+  //创建dom
+  let ele = document.createElement();
+  let attrName;
+  //给dom添加元素
+  for (attrName in attr) {
+    if (attr.hasOwnProperty(attrName)) {
+      ele.setAttribute(attrName, attr[attrName]);
+    }
   }
+  //将子元素添加到dom上
+  children.forEach((children) => {
+    ele.appendChild(createNewNode(children));
+  });
+
+  return ele;
+}
+
+//简单实现vue的更新方法，递归实现
+function update(vNode, newNode) {
+  let children = vNode.children || {};
+  let newChildren = newNode.children || {};
+  children.forEach((childrenVnode, index) => {
+    let newChildrenVnode = newChildren[index];
+    if (childrenVnode.tag == newChildrenVnode.tag) {
+      update(childrenVnode, newChildrenVnode);
+    } else {
+      replace(childrenVnode, newChildrenVnode);
+    }
+  });
+}
