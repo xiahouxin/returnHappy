a068b648c7711bce4598b3f0164f32f552df89a8
xiahouxin <2786724159@qq.com>
2023-04-08 21:18:19 +0800

    快速排序

--- 排序算法/1.js
+++ 排序算法/1.js
@@ -1,33 +1,33 @@
 let arr = [13, 24, 12, 3, 23, 9, 2];
-      // function insertSort(arr) {
-      //   let len = arr.length;
-      //   let preIndex, currentValue;
-      //   for (let i = 1; i < len; i++) {
-      //     preIndex = i - 1;
-      //     currentValue = arr[i];
-      //     while (preIndex >= 0 && arr[preIndex] > currentValue) {
-      //       arr[preIndex + 1] = arr[preIndex];
-      //       preIndex--;
-      //     }
-      //     arr[preIndex + 1] = currentValue;
-      //   }
-      //   return arr;
-      // }
+// function insertSort(arr) {
+//   let len = arr.length;
+//   let preIndex, currentValue;
+//   for (let i = 1; i < len; i++) {
+//     preIndex = i - 1;
+//     currentValue = arr[i];
+//     while (preIndex >= 0 && arr[preIndex] > currentValue) {
+//       arr[preIndex + 1] = arr[preIndex];
+//       preIndex--;
+//     }
+//     arr[preIndex + 1] = currentValue;
+//   }
+//   return arr;
+// }
 
-      //[13, 24, 12, 3, 23, 9, 2];
-      function insertSort(arr) {
-        let len = arr.length;
-        let preIndex, currentValue;
-        for (let i = 1; i < len; i++) {
-          preIndex = i - 1;
-          currentValue = arr[i];
-          // console.log(arr[preIndex], currentValue);
-          while (preIndex >= 0 && arr[preIndex] > currentValue) {
-            arr[preIndex + 1] = arr[preIndex];
-            preIndex--;
-          }
-          arr[preIndex + 1] = currentValue;
-        }
-        return arr;
-      }
-      console.log(insertSort(arr));
+//[13, 24, 12, 3, 23, 9, 2];
+function insertSort(arr) {
+  let len = arr.length;
+  let preIndex, currentValue;
+  for (let i = 1; i < len; i++) {
+    preIndex = i - 1;
+    currentValue = arr[i];
+    // console.log(arr[preIndex], currentValue);
+    while (preIndex >= 0 && arr[preIndex] > currentValue) {
+      arr[preIndex + 1] = arr[preIndex];
+      preIndex--;
+    }
+    arr[preIndex + 1] = currentValue;
+  }
+  return arr;
+}
+console.log(insertSort(arr));
