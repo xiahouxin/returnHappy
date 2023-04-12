587bee90becc666f2f4c4d13038d6a515e163c66
xiahouxin <2786724159@qq.com>
2023-04-12 15:52:28 +0800

    去除重复字母

---
+++ LeetCode/贪心/316.js
@@ -0,0 +1,25 @@
+var removeDuplicateLetters = function(s) {
+    const vis = new Array(26).fill(0);
+    const num = _.countBy(s);
+    
+    const sb = new Array();
+    for (let i = 0; i < s.length; i++) {
+        const ch = s[i];
+        if (!vis[ch.charCodeAt() - 'a'.charCodeAt()]) {
+            while (sb.length > 0 && sb[sb.length - 1] > ch) {
+                if (num[sb[sb.length - 1]] > 0) {
+                    vis[sb[sb.length - 1].charCodeAt() - 'a'.charCodeAt()] = 0;
+                    sb.pop();
+                } else {
+                    break;
+                }
+            }
+            vis[ch.charCodeAt() - 'a'.charCodeAt()] = 1;
+            sb.push(ch);
+        }
+        num[ch]--;
+    }
+    return sb.join('');
+};
+
+console.log(removeDuplicateLetters("cbacdcbc"));
