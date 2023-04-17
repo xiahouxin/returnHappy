b4a48e8044754a86a63ee8d3c2f277cd6651fe96
xiahouxin <2786724159@qq.com>
2023-04-17 10:42:11 +0800

    async-await的执行机制

---
+++ js/event-loop/Event-loop/index.js
@@ -0,0 +1,61 @@
+console.log("script start");
+// async表示当前这个函数内部可以存在异步（await）
+async function async1() {
+  await async2(); //加了await的代码会立即执行，且后面的代码会阻塞（会导致后面的代码去到下一次的微任务队列中）
+  console.log("async1 end");
+}
+async function async2() {
+  console.log("async2 end");
+}
+async1()
+setTimeout(function () {
+  console.log("setTimeout");
+}, 0);
+new Promise((resolve) => {
+  console.log("Promise");
+  resolve();
+})
+  .then(function () {
+    console.log("promise1");
+  })
+  .then(function () {
+    console.log("promise2");
+  });
+// async1();
+console.log("script end");
+
+// script start
+// async2 end
+// Promise
+// script end
+// ----本来在这边v8违反规定，将await后面的代码提上一个循环的微任务队列
+// promise1
+// promise2
+// setTimeout
+// async1 end----
+
+// 必问
+
+//     #浏览器中的event- _Loop
+//     当j s代码在执行时，就是往调用栈中放进去函数，再执行，但是!如果遇到异步函数，异步函数就会被挂起，并在调用栈为空的时候拿出来执行。
+
+//     从任务队列中提取函数出来执行是分两种情况:
+// 1.微任务 (microtask)
+
+// 2.宏任务(macrotask)
+
+// 微任务: process. nextTick, prrse. then, Mutat ionObserver
+
+// 宏任务: script, setTimeout, setInterval, setImmediate, I/0, UI- rendering
+
+// # . event-loop顺序:
+
+// 1.首先执行同步代码，这属于宏任务
+
+// 2.当执行完所有的同步代码后，执行栈为空， 查询是否有异步代码要执行
+
+// 3.执行所有的微任务
+
+// 4.当所有的微任务执行完毕之后，有需要的话会渲染页面
+
+// 5.开启下一次的Event-Loop,执行宏任务中的异步代码
--- js/event-loop/loop.js
+++ js/event-loop/loop.js
@@ -4,10 +4,15 @@ async function async1() {
   console.log(arr);
   console.log('async1 end')
 }
-async function async2() {                      //async会影响答案
+async function async2() { 
+  // console.log('async2 end')
+  // return "okk"                     //async会影响答案
   return new Promise((resolve,reject)=>{
       console.log('async2 end')
       resolve('okk')
+  }).then(res=>{   //此处加不加then结果一样的
+    console.log('async2返回值内部then');
+    return res
   })
 }
 async1()
@@ -27,13 +32,24 @@ new Promise(resolve => {
   .then(function (res) {
     console.log('promise3',res)  //res==undefined
   })
+  .then(function (res) {
+    console.log('promise4',res)  //res==undefined
+  })
+  .then(function (res) {
+    console.log('promise5',res)  //res==undefined
+  })
 console.log('script end')
+
+// async2函数有async       没有async
 
 // script start
 // async2 end
 // Promise
 // script end
-// async1 end -----
-// promise1
-// promise2
+// async2返回值内部then
+// promise1 123           okk
+// promise2 undefined     async1 end
+// okk                    promise1 123
+// async1 end             promise2 undefined
+// promise3 undefined     promise3 undefined
 // setTimeout
