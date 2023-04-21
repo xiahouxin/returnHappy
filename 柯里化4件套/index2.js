082216281d6131af39276c0a603acbd63bd9db0f
xiahouxin <2786724159@qq.com>
2023-04-21 20:08:24 +0800

    柯里化

--- 柯里化4件套/index2.js
+++ 柯里化4件套/index2.js
@@ -69,48 +69,48 @@
 
 
 
-function sub_curry(fn){
-    let args=Array.prototype.slice.call(arguments,1)
-    return function(){
-      let newArgs=args.concat([...arguments])
-      return fn.apply(this,newArgs)
+// function sub_curry(fn) {
+//   let args = Array.prototype.slice.call(arguments, 1)
+//   return function () {
+//     let newArgs = args.concat([...arguments])
+//     return fn.apply(this, newArgs)
+//   }
+// }
+
+// function curry(fn, length) {
+//   length = length || fn.length
+//   let slice = Array.prototype.slice
+//   return function () {
+//     if (arguments.length < length) {
+//       let cback = [fn].concat(slice.call(arguments))
+//       return curry(sub_curry.apply(this, cback), length - arguments.length)
+//     } else {
+//       return fn.apply(this, arguments)
+//     }
+//   }
+// }
+
+// 通用版
+function curry(fn, args) {
+  var length = fn.length;
+  var args = args || [];
+  return function () {
+    newArgs = args.concat(Array.prototype.slice.call(arguments))
+    console.log(newArgs)
+    if (newArgs.length < length) {
+      return curry.call(this, fn, newArgs);
+    } else {
+      return fn.apply(this, newArgs);
     }
   }
-  
-  function curry(fn,length){
-    length=length || fn.length
-    let slice=Array.prototype.slice
-    return function(){
-      if(arguments.length<length){
-        let cback=[fn].concat(slice.call(arguments))
-        return curry(sub_curry.apply(this,cback),length-arguments.length)
-      }else{
-        return fn.apply(this,arguments)
-      }
-    }
-  }
-  
-   // 通用版
-   function curry(fn, args) {
-    var length = fn.length;
-    var args = args || [];
-    return function() {
-        newArgs = args.concat(Array.prototype.slice.call(arguments))
-        console.log(newArgs)
-        if(newArgs.length < length) {
-            return curry.call(this, fn, newArgs);
-        } else {
-            return fn.apply(this, newArgs);
-        }
-    }
-  }
-  function multiFn(a, b, c) {
-    console.log(a * b * c)
-    return a * b * c;
-  }
-  
-  var multi = curry(multiFn);
-  multi(2)(3)(4);
+}
+function multiFn(a, b, c) {
+  console.log(a * b * c)
+  return a * b * c;
+}
+
+var multi = curry(multiFn);
+multi(2)(3)(4);
   // multi(2, 3, 4)
   // multi(2)(3, 4)
   // multi(2, 3)(4)
