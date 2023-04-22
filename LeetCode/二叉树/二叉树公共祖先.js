5c43d6625b7d70f267e30a563ad41f25aa70f3e0
xiahouxin <2786724159@qq.com>
2023-04-22 19:42:32 +0800

    二叉树公共祖先

--- LeetCode/二叉树/二叉树公共祖先.js
+++ LeetCode/二叉树/二叉树公共祖先.js
@@ -11,10 +11,28 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-var lowestCommonAncestor = function(root, p, q) {
-    if(root.val < p.val && root.val < q.val)
+// 递归
+var lowestCommonAncestor = function (root, p, q) {
+    if (root.val < p.val && root.val < q.val)
         return lowestCommonAncestor(root.right, p, q);
-    if(root.val > p.val && root.val > q.val)
+    if (root.val > p.val && root.val > q.val)
         return lowestCommonAncestor(root.left, p, q);
     return root;
 };
+
+// 迭代
+function lowestCommonAncestor( root,  p,  q) {
+    if(p.val > q.val) { // 保证 p.val < q.val
+        let tmp = p;
+        p = q;
+        q = tmp;
+    }
+    while(root != null) {
+        if(root.val < p.val) // p,q 都在 root 的右子树中
+            root = root.right; // 遍历至右子节点
+        else if(root.val > q.val) // p,q 都在 root 的左子树中
+            root = root.left; // 遍历至左子节点
+        else break;
+    }
+    return root;
+}
