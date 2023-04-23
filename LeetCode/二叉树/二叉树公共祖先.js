87972bc9e5182a937f9cd57e03f8a3406e4458cb
xiahouxin <2786724159@qq.com>
2023-04-23 16:02:12 +0800

    无序二叉树公共祖先

--- LeetCode/二叉树/二叉树公共祖先.js
+++ LeetCode/二叉树/二叉树公共祖先.js
@@ -41,3 +41,34 @@ function lowestCommonAncestor( root,  p,  q) {
 
 
 // // 以下是二叉树节点没有大小之分--------------------------------
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if(root == null || root == p || root == q) return root;
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+    if(left == null) return right;
+    if(right == null) return left;
+    return root;
+    
+    // if(root == null || root == p || root == q) return root;
+    // let left = lowestCommonAncestor(root.left, p, q);
+    // let right = lowestCommonAncestor(root.right, p, q);
+    // if(left == null && right == null) return null; // 1.
+    // if(left == null) return right; // 3.
+    // if(right == null) return left; // 4.
+    // return root; // 2. if(left != null and right != null)
+};
