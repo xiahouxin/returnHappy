a3cfa7738ee48b810b2c2eb2032bd2d9d37f34c5
xiahouxin <2786724159@qq.com>
2023-05-02 16:03:39 +0800

    async-await手写

--- js/async-await/index.js
+++ js/async-await/index.js
@@ -1,7 +1,7 @@
-function request(num){
-    return new Promise(resolve=>{
+function request(num) {
+    return new Promise(resolve => {
         setTimeout(() => {
-            resolve(num*2)
+            resolve(num * 2)
             // console.log(new Date());
             // console.log(num*2 ,'111');
         }, 2000);
@@ -33,12 +33,12 @@ function request(num){
 // }
 // fn()
 
-async function fn(){
-    const res1=await request(5)
-    console.log(res1,'res1');
-    const res2=await request(res1)
-    console.log(res2,'res2');
-    return res2+2
+async function fn() {
+    const res1 = await request(5)
+    console.log(res1, 'res1');
+    const res2 = await request(res1)
+    console.log(res2, 'res2');
+    return res2 + 2
 }
 // function fn(){  //没有await的话，会在一秒内执行完
 //     const res1=request(5)
@@ -48,7 +48,7 @@ async function fn(){
 // fn()
 
 // async function fn(){      //async函数返回的是promise对象，如果async内部有返回值，则返回值就是promise对象里的reslove里面的值，可以被then接收到,
-                             //await 后面如果接的是promise对象，这这个promise对象里的reslove函数值就是await接收到的值，如果这个promise对象没有reslove，则无法接收到值，且影响后面代码执行
+//await 后面如果接的是promise对象，这这个promise对象里的reslove函数值就是await接收到的值，如果这个promise对象没有reslove，则无法接收到值，且影响后面代码执行
 //     const res1=await 5    //await 后面如果接收的是基本类型，则res1就是这个基本类型，即5
 //     const res2=await res1+3
 //     console.log(res2);
@@ -57,9 +57,8 @@ async function fn(){
 // fn()
 
 // async function fn(num){   //async函数返回的是promise对象，如果async内部有返回值，则返回值就是promise对象里的reslove里面的值，可以被then接收到,
-                             //await 后面如果接的是promise对象，这这个promise对象里的reslove函数值就是await接收到的值
 //     return num
 // }
 console.log(fn);
 // console.log(fn(10));
-fn().then(res=> console.log(res))
+fn().then(res => console.log(res))
--- js/async-await/promiseNext.js
+++ js/async-await/promiseNext.js
@@ -1,33 +1,33 @@
-function fn(nums){
-    return new Promise(reslove=>{
+function fn(nums) {
+    return new Promise(reslove => {
         setTimeout(() => {
-            reslove(nums*2)
+            reslove(nums * 2)
         }, 1000);
     })
 
 }
 
-function* gen(){
-    const num1=yield fn(1)  //1
-    console.log(num1,'num1111');
-    const num2=yield fn(num1) //
-    const num3=yield fn(num2)  //
+function* gen() {
+    const num1 = yield fn(1)  //1
+    console.log(num1, 'num1111');
+    const num2 = yield fn(num1) //
+    const num3 = yield fn(num2)  //
     return num3
 }
-const g=gen()
-const next1=g.next()
+const g = gen()
+const next1 = g.next()
 console.log(next1);  //{ value: Promise { <pending> }, done: false }
-next1.value.then(res1=>{
+next1.value.then(res1 => {
     console.log(next1)    //{ value: Promise { 2 }, done: false }
     console.log(res1);  //2
 
-    const next2=g.next(res1+3)   //res1+3给了num1
-    next2.value.then(res2=>{
+    const next2 = g.next(res1 + 3)   //res1+3给了num1
+    next2.value.then(res2 => {
         console.log(next2);
         console.log(res2);  //10
 
-        const next3=g.next(res2)
-        next3.value.then(res3=>{
+        const next3 = g.next(res2)
+        next3.value.then(res3 => {
             console.log(next3);
             console.log(res3);
 
