c876f944dd1b4b0220340e184e20200d486a11c3
xiahouxin <2786724159@qq.com>
2023-05-05 14:51:11 +0800

    快慢指针思想

---
+++ LeetCode/快慢指针/环形链表.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */        
+//         力扣原型142
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let fast=head,slow=head;
+    while(true){
+        if(fast==null||fast.next==null) return null;
+        fast=fast.next.next;
+        slow=slow.next;
+        if(slow==fast){
+            break;
+        }
+    }
+    fast=head;
+    while(fast!=slow){
+        fast=fast.next;
+        slow=slow.next;
+    }
+    return fast;
+};
+
+// 第一次相遇fast走的是slow的两倍，所以第一次相遇时 慢指针已经走了nb步   且关键点在于 走a+nb步一定是在环入口
