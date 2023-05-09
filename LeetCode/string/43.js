fb8ace6041abd1b433a24c3682430ebd12af0925
xiahouxin <2786724159@qq.com>
2023-05-09 09:44:39 +0800

    string

--- LeetCode/string/43.js
+++ LeetCode/string/43.js
@@ -3,12 +3,11 @@
  * @param {string} num2
  * @return {string}
  */
-var multiply = function(num1, num2) {
-    if (num1=="0" || num2=="0") {
+var multiply = function (num1, num2) {
+    if (num1 == "0" || num2 == "0") {
         return "0";
     }
     let res = new Array(num1.length + num2.length).fill(0);
-    // console.log(res);
     for (let i = num1.length - 1; i >= 0; i--) {
         let n1 = parseInt(num1[i]);
         for (let j = num2.length - 1; j >= 0; j--) {
@@ -21,10 +20,10 @@ var multiply = function(num1, num2) {
 
     let result = "";
     for (let i = 0; i < res.length; i++) {
-        console.log(res[i],i);
+        console.log(res[i], i);
         if (i == 0 && res[i] == 0) continue;
-        result+=res[i]
+        result += res[i]
     }
     return result;
 };
-console.log(multiply('2','3'));
+console.log(multiply('2', '3'));
