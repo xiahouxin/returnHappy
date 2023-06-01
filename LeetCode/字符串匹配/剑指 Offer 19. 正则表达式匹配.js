c2a231ed1665132a0b6bb8fcf211d028ad55ee09
xiahouxin <2786724159@qq.com>
2023-06-01 21:23:41 +0800

    字符串匹配

--- LeetCode/字符串匹配/剑指 Offer 19. 正则表达式匹配.js
+++ LeetCode/字符串匹配/剑指 Offer 19. 正则表达式匹配.js
@@ -3,13 +3,13 @@
  * @param {string} p
  * @return {boolean}
  */
-var isMatch = function(s, p) {
+var isMatch = function (s, p) {
     let m = s.length;
     let n = p.length;
 
     let f = new Array(m + 1);
-    for(let i=0;i<=m;i++){
-        f[i]=new Array(n+1).fill(false)
+    for (let i = 0; i <= m; i++) {
+        f[i] = new Array(n + 1).fill(false)
     }
     f[0][0] = true;
     for (let i = 0; i <= m; ++i) {
@@ -29,7 +29,7 @@ var isMatch = function(s, p) {
     return f[m][n];
 }
 
-function matches(s, p,  i,  j) {
+function matches(s, p, i, j) {
     if (i == 0) {
         return false;
     }
