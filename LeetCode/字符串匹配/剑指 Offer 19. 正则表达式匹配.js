94649749cdd9927fe60ee53b8c84b00ad695f4cf
xiahouxin <2786724159@qq.com>
2023-06-01 21:06:12 +0800

    字符串匹配

---
+++ LeetCode/字符串匹配/剑指 Offer 19. 正则表达式匹配.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+    let m = s.length;
+    let n = p.length;
+
+    let f = new Array(m + 1);
+    for(let i=0;i<=m;i++){
+        f[i]=new Array(n+1).fill(false)
+    }
+    f[0][0] = true;
+    for (let i = 0; i <= m; ++i) {
+        for (let j = 1; j <= n; ++j) {
+            if (p.charAt(j - 1) == '*') {
+                f[i][j] = f[i][j - 2];
+                if (matches(s, p, i, j - 1)) {
+                    f[i][j] = f[i][j] || f[i - 1][j];
+                }
+            } else {
+                if (matches(s, p, i, j)) {
+                    f[i][j] = f[i - 1][j - 1];
+                }
+            }
+        }
+    }
+    return f[m][n];
+}
+
+function matches(s, p,  i,  j) {
+    if (i == 0) {
+        return false;
+    }
+    if (p.charAt(j - 1) == '.') {
+        return true;
+    }
+    return s.charAt(i - 1) == p.charAt(j - 1);
+}
