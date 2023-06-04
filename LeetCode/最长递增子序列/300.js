2abdd0511e7d6d9ff885556aa73cdaa99a98d327
xiahouxin <2786724159@qq.com>
2023-06-04 18:07:34 +0800

    最长增长子序列

---
+++ LeetCode/最长递增子序列/300.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    //    let len=nums.length
+    //     if(len<2){
+    //         return len
+    //     }
+    //     let tail=new Array(len)
+    //     tail[0]=nums[0]
+    //     let end=0
+    //     for(let i=1;i<len;i++){
+    //         if(nums[i]>tail[end]){
+    //             tail[++end]=nums[i]
+    //         }else{
+    //             let l=-1,r=end+1
+    //             while(l+1!=r){
+    //                 let m=~~((l+r)/2)
+    //                 if(tail[m]>=nums[i]){
+    //                     r=m
+    //                 }else{
+    //                     l=m
+    //                 }
+    //             }
+    //             tail[r]=nums[i]
+    //         }
+    //     }
+    //     return ++end
+        let len=nums.length
+        if(len<2){
+            return len
+        }
+        let res=1
+        let dp=new Array(len).fill(1)
+        for(let i=1;i<len;i++){
+            for(let j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1)
+                }
+            }
+            res=Math.max(res,dp[i])
+        }
+        return res
+    };
