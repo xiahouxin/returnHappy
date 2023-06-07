cb1f6b3a3f5d31170888764c2e6a5505330abda6
xiahouxin <2786724159@qq.com>
2023-06-07 15:57:04 +0800

    string

--- LeetCode/string/reverse.js
+++ LeetCode/string/reverse.js
@@ -1,17 +1,17 @@
 // 翻转字符串
 // 递归
 var line = readline();
-var reverse = (str)=>{
-    if(str.length<1) return str;
-    return reverse(str.slice(1))+str[0];
+var reverse = (str) => {
+    if (str.length < 1) return str;
+    return reverse(str.slice(1)) + str[0];
 }
 console.log(reverse(line))
 
 // 双指针
 var line = readline();
-var reverse = (str)=>{
-    if(str.length<1) return str;
-    return reverse(str.slice(1))+str[0];
+var reverse = (str) => {
+    if (str.length < 1) return str;
+    return reverse(str.slice(1)) + str[0];
 }
 console.log(reverse(line))
 
