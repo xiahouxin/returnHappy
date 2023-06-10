beffe463fa42ec89386a3e77303a4b6a65856249
xiahouxin <2786724159@qq.com>
2023-06-10 21:40:27 +0800

    dfs

--- LeetCode/dfs/513找树左下角的值.js
+++ LeetCode/dfs/513找树左下角的值.js
@@ -1,5 +1,4 @@
 // 给定一个二叉树的 根节点 root，请找出该二叉树的 最底层 最左边 节点的值。
-
 // 假设二叉树中至少有一个节点。
 
 // 示例 1:
@@ -20,26 +19,26 @@
  * @return {number}
  */
 // dfs
-var findBottomLeftValue = function(root) {
-    let maxPath=0,resultVal=null
-    const dfsTree=function(node,curPath){
-        if(node.left==null&&node.right==null){
-            if(curPath>maxPath){
-                maxPath=curPath
-                resultVal=node.val
+var findBottomLeftValue = function (root) {
+    let maxPath = 0, resultVal = null
+    const dfsTree = function (node, curPath) {
+        if (node.left == null && node.right == null) {
+            if (curPath > maxPath) {
+                maxPath = curPath
+                resultVal = node.val
             }
             return
         }
-        node.left&&dfsTree(node.left,curPath+1)
-        node.right&&dfsTree(node.right,curPath+1)
+        node.left && dfsTree(node.left, curPath + 1)
+        node.right && dfsTree(node.right, curPath + 1)
     }
-    dfsTree(root,1)
+    dfsTree(root, 1)
     return resultVal
 };
 
 
 // bfs
-var findBottomLeftValue = function(root) {
+var findBottomLeftValue = function (root) {
     let ret = 0;
     const queue = [root];
     while (queue.length) {
